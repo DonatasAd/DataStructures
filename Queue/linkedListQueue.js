2864dfad28266f291f2beb084ddf2ba6cfe4a635
Donatas <adamonis@mail.com>
2020-09-24 20:04:07 +0300

    queue updated

--- Queue/linkedListQueue.js
+++ Queue/linkedListQueue.js
@@ -1,4 +1,3 @@
-//Node
 class Node {
   constructor(element) {
     this.element = element;
@@ -10,101 +9,34 @@ class Queue {
   constructor() {
     this.length = 0;
     this.head = null;
+    this.tail = null;
   }
 
   //Push data in the queue
+  // complexity - O(1)
   enqueue(element) {
-    let node = new Node(element),
-      current;
-
-    //If head is empty then
-    //Add the node at the beginning
-    if (this.head === null) {
-      this.head = node;
+    let newNode = new Node(element);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
     } else {
-      //Else add the node as the
-      //Next element of the existing list
-      current = this.head;
-      while (current.next) {
-        current = current.next;
-      }
-
-      current.next = node;
+      this.tail.next = newNode;
+      this.tail = newNode;
     }
-
-    //Increase the length
-    this.length++;
+    return this.length++;
   }
 
   //Remove the item from the queue
+  // complexity - O(1)
   dequeue() {
-    let current = this.head;
+    if (!this.head) return null;
+    let temp = this.head;
 
-    //If there is item then remove it
-    //and make the next element as the first
-    if (current) {
-      let element = current.element;
-      current = current.next;
-      this.head = current;
-      this.length--;
-      return element;
+    if (this.head === this.tail) {
+      this.tail = null;
     }
-
-    return null;
-  }
-
-  //Return the first element in the queue
-  front() {
-    if (this.head) {
-      return this.head.element;
-    }
-
-    return null;
-  }
-
-  //Return the last element in the queue
-  rear() {
-    let current = this.head;
-
-    //If head is empty
-    //Return null
-    if (current === null) {
-      return null;
-    }
-
-    //Return the last elememnt
-    while (current.next) {
-      current = current.next;
-    }
-
-    return current.element;
-  }
-
-  //Convert the queue to an array
-  toArray() {
-    let arr = [];
-    let current = this.head;
-    while (current) {
-      arr.push(current.element);
-      current = current.next;
-    }
-
-    return arr;
-  }
-
-  //Check if stack is empty
-  isEmpty() {
-    return this.length === 0;
-  }
-
-  //Return the size of the stack
-  size() {
-    return this.length;
-  }
-
-  //Clear the stack
-  clear() {
-    this.head = null;
-    this.length = 0;
+    this.head = this.head.next;
+    this.length--;
+    return temp.element;
   }
 }
